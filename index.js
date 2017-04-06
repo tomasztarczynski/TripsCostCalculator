6716bdb4d572db0da7de2b2ce51d046298f62b36
Tomasz Tarczyński <tomasz.tarczynski.job@outlook.com>
2017-04-06 12:38:52 +0200

    Add demo to index

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 'use strict';
 
-console.log('**** Trips cost calculator ****');
+var TripTokenParser = require('./src/trip-token-parser');
+var TripRowParser = require('./src/trip-row-parser');
+var TripParser = require('./src/trip-parser');
+
+const util = require('util')
+
+console.log('**** Trips cost calculator ****\n\n');
+
+var rawTrips = [
+    '##### 1 #####',
+    '',
+    'Sunday 1.1.2017:',
+    '',
+    '##### 2 #####',
+    '',
+    'Monday 2.1.2017: J!T#P/2.R/2',
+    'Tuesday 3.1.2017: JwA!T#R',
+    'Wednesday 4.1.2017: CZ!T#R',
+    'Thursday 5.1.2017: JwCZ!T#R',
+    'Friday 6.1.2017: J!P#R.T',
+    '',
+    'Saturday 7.1.2017:',
+    'Sunday 8.1.2017:'
+].join('\r\n');
+
+var tripTokenParser = new TripTokenParser();
+var tripRowParser = new TripRowParser(tripTokenParser);
+var tripParser = new TripParser(tripRowParser);
+
+var trips = tripParser.parse(rawTrips);
+
+console.log(util.inspect(trips, false, null))
